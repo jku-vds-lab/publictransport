9794ab0dbee4ae1638c2080d560f12fe221c0410
Christina <ginihumer96@yahoo.de>
2023-04-04 08:32:11 +0200

    fix IP

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -2,7 +2,8 @@ import './app.css'
 import App from './App.svelte'
 //let map: google.maps.Map;
 
-export const BASE_URL = "http://127.0.0.1:9000/"
+// export const BASE_URL = "http://127.0.0.1:9000/" // for local development
+export const BASE_URL = "" // for AWS/docker
 
 const app = new App({
   target: document.getElementById('app'),
