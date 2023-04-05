b9ec3f654112f353b0b403feeb9d8d55fc58b2ef
MarkusTrainer <96826589+MarkusTrainer@users.noreply.github.com>
2023-04-05 20:48:16 +0200

    Update App.svelte

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,6 +1,18 @@
 <script>
   import Navbar from './lib/Navbar.svelte'
   import Map from './lib/Map.svelte' 
+  import { locale, register, init, getLocaleFromNavigator, addMessages } from 'svelte-i18n';
+
+    import en from './localisation/en.json';
+    import de from './localisation/de.json';
+
+    addMessages('en', en);
+    addMessages('de', de);
+
+    init({
+      fallbackLocale: 'en',
+      initialLocale: getLocaleFromNavigator(),
+    });
 </script>
 
 <main>
