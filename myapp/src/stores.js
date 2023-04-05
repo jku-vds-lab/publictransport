cbee4b54ff41ff0dfc6a0d7bcefe2520fc359eb1
MarkusTrainer <96826589+MarkusTrainer@users.noreply.github.com>
2023-04-05 20:46:55 +0200

    Update stores.js

--- myapp/src/stores.js
+++ myapp/src/stores.js
@@ -5,3 +5,4 @@ export const marker = writable("should be marker")
 export const selectedOption = writable("should be selectedOption")
 export const currentTime = writable("should be currentTime")
 export const currentMinutes = writable("should be currentMinutes")
+export const isOpen = writable(true);
