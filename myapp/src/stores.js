00ad05ee49a0681345ddcf17dfd98b684138c5a7
MarkusTrainer <markustrainer00@gmail.com>
2023-06-11 20:02:12 +0200

    final protoype v1.1

--- myapp/src/stores.js
+++ myapp/src/stores.js
@@ -3,6 +3,7 @@ import { writable } from 'svelte/store';
 export const url_stored = writable("should be url")
 export const marker = writable("should be marker")
 export const selectedOption = writable("should be selectedOption")
+export const selectedOptionB = writable("should be selectedOption")
 export const currentTime = writable("should be currentTime")
 export const currentMinutes = writable("should be currentMinutes")
 export const isOpen = writable(true);
